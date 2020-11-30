da65ca733dc282cff620b7b77f2485ddd50d2806
benzigar-codes <36362957+benzigar-codes@users.noreply.github.com>
2020-11-30 09:48:58 +0530

    Added details in ADMIN

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,5 +3,10 @@
 @section('title','Admin')
 
 @section('content')
-	<h1>Admin ,, Welcome you hereeeee</h1>
+	<div class="min-h-screen flex">
+		<div class="hidden lg:block lg:w-1/5 bg-green-800">
+			<h1 class="font-bold text-xl bg-green-900 text-white w-full py-4 px-3 flex items-center border-b-4 border-green-500"><div class="h-10 w-10 bg-green-800 rounded-full mr-3"></div>Kanyakumariyan</h1>
+		</div>
+		<div class="w-full lg:w-4/5 bg-gray-200"></div>
+	</div>
 @endsection
